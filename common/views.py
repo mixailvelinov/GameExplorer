d4af4f78c33b204129c3f242b210a91acee177e0
mixailvelinov <mihail.vasilev.velinov@gmail.com>
2024-11-27 00:18:29 +0200

    Finalized Index and game review templates.

--- common/views.py
+++ common/views.py
@@ -1,6 +1,10 @@
+from django.db.models import Avg, Sum
+from django.db.models.functions import Coalesce
+from django.forms import FloatField
 from django.shortcuts import render
 
 from accounts.models import Profile
+from games.models import Game
 
 
 # Create your views here.
@@ -17,8 +21,20 @@ def index(request):
         account = None
         profile = None
 
+    latest_game = Game.objects.order_by('-release_date').first()
+
+    top_game = Game.objects.annotate(
+        average_rating=Avg('review__rating')
+    ).filter(average_rating__isnull=False).order_by('-average_rating').first()
+
+    games = Game.objects.all()
+
     context = {
         'account': account,
         'profile': profile,
+        'latest_game': latest_game,
+        'top_game': top_game,
+        'games': games,
     }
     return render(request, 'common/index.html', context)
+
