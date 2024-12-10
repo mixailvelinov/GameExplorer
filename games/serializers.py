9dc0ee35c0e8e177195d0ad0dec572b56ed013b0
mixailvelinov <mihail.vasilev.velinov@gmail.com>
2024-12-10 16:51:52 +0200

    fixed: fixed game serializer and added code to templates

--- games/serializers.py
+++ games/serializers.py
@@ -40,16 +40,26 @@ class GameSerializer(serializers.ModelSerializer):
         platforms_data = validated_data.pop('platform', None)
         if platforms_data:
             instance.platform.clear()
-            for platform in platforms_data:
-                platform, created = Platform.objects.get_or_create(platform_name=platform['platform_name'])
-                instance.platform.add(platform)
+            for platform_data in platforms_data:
+                platform_name = platform_data['platform_name']
+                platform = Platform.objects.filter(platform_name=platform_name).first()
+                if platform:
+                    instance.platform.add(platform)
+                else:
+                    platform = Platform.objects.create(platform_name=platform_name)
+                    instance.platform.add(platform)
 
         genres_data = validated_data.pop('genre', None)
         if genres_data:
             instance.genre.clear()
-            for genre in genres_data:
-                genre, created = Genre.objects.get_or_create(genre_name=genre['genre_name'])
-                instance.genre.add(genre)
+            for genre_data in genres_data:
+                genre_name = genre_data['genre_name']
+                genre = Genre.objects.filter(genre_name=genre_name).first()
+                if genre:
+                    instance.genre.add(genre)
+                else:
+                    genre = Genre.objects.create(genre_name=genre_name)
+                    instance.genre.add(genre)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
