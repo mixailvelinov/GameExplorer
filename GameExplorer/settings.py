9cca917a204d374c08f0acfdb2a18e3def58cebe
mixailvelinov <mihail.vasilev.velinov@gmail.com>
2024-12-10 03:10:09 +0200

    fix: csrf

--- GameExplorer/settings.py
+++ GameExplorer/settings.py
@@ -34,7 +34,7 @@ allowed_hosts_str = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS', default='
 ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_str.split(',') if host.strip()]
 
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 INSTALLED_APPS = [
